34e5efd2d70e3cf40af3dfc7e118152e1bb70706
MarioSergio2 <ms940054@gmail.com>
2024-08-02 22:25:34 -0300

    Resolvendo exercicios MapAndSet
    Signed-off-by: MarioSergio2 <ms940054@gmail.com>

---
+++ src/main/java/map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,57 @@
+package main.java.map.Ordenacao;
+
+import jdk.jfr.Event;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, evento);
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        /*Set<LocalDate> dateSet = eventosMap.keySet();
+        Collection<Evento> values = eventosMap.values();*/
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData  = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for(Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O proximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.JULY, 15), "Evento 1", "Atracao 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atracao 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atracao 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.AUGUST, 02), "Evento 4", "Atracao 4");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+
+    }
+}
---
+++ src/main/java/map/Pesquisa/EstoqueProdutos.java
@@ -0,0 +1,57 @@
+package main.java.map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EstoqueProdutos {
+    //atributo
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProdutos(Long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos() {
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        double valorTotalEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        for (Produto p : estoqueProdutosMap.values()) {
+            if (p.getPreco() > maiorPreco) {
+                produtoMaisCaro = p;
+                maiorPreco = p.getPreco();
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoqueProdutos = new EstoqueProdutos();
+        estoqueProdutos.exibirProdutos();
+
+        estoqueProdutos.adicionarProdutos(1L, "Produto A", 10, 5.0);
+        estoqueProdutos.adicionarProdutos(2L, "Produto B", 5, 10.0);
+        estoqueProdutos.adicionarProdutos(3L, "Produto C", 2, 15.0);
+
+        estoqueProdutos.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoqueProdutos.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: " + estoqueProdutos.obterProdutoMaisCaro());
+    }
+}
